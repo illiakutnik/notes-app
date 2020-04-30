3b86f02e5606f6e340ae8da77391fa32478b6067
Illia KUtnik <illiakutnik@gmail.com>
2020-04-30 16:13:26 +0300

    change

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -52,7 +52,7 @@ const Note = ({ selectedNote, notes, editNote, closeModal }) => {
             <ArrowBackIcon />
           </IconButton>
           <p className={classes.mobileUpText}>
-            your changes is going be save automatically, it's ok!!!
+            your changes is will be saved automatically
           </p>
         </div>
       )}
